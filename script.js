25c20dfb2ae0d62cb0243e8f88bb7f5069bfe1ad
NemishGorasiya <nemish.gorasiya@zuru.com>
2024-01-30 15:51:32 +0530

    Validation added in signIn & password eye button

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 let hidden = document.querySelectorAll(".hidden")
 let buttons = document.querySelectorAll(".fa-plus");
 
+
+
+let eye_btn = document.getElementById("eye-btn");
+let password = document.getElementById("signin-password");
+eye_btn.addEventListener("click",()=>{
+    eye_btn.classList.toggle("fa-eye-slash");
+    eye_btn.classList.toggle("fa-eye");
+    if (password.type === "password") {
+        password.type = "text";
+    }else {
+        password.type = "password";
+    }
+})
+
+
 buttons.forEach((ele,index) => {
     ele.addEventListener("click",()=>{ 
         if(ele.classList.contains("fa-close")){
@@ -19,13 +34,57 @@ buttons.forEach((ele,index) => {
     })
 });
 
-// .hidden-content {
-//     max-height: 0;
-//     overflow: hidden;
-//     transition: max-height 0.3s ease-out; /* Adjust the duration and timing function as needed */
-// }
+let signin_email = document.getElementById("signin-email");
+let signin_password = document.getElementById("signin-password");
+let email_error = document.getElementById("email-error");
+let password_error = document.getElementById("password-error");
 
-// .visible-content {
-//     max-height: 500px; /* Set a reasonable max-height value */
-//     transition: max-height 0.3s ease-in; /* Adjust the duration and timing function as needed */
-// }
+signin_email.addEventListener("input",()=>{
+    console.log(ValidateEmail(signin_email.value));
+    if (!ValidateEmail(signin_email.value)) {
+        email_error.style.display = "block";
+    }else {
+        email_error.style.display = "none";
+    }
+})
+signin_password.addEventListener("input",()=>{
+    console.log(ValidatePassword(signin_password.value));
+    if(!ValidatePassword(signin_password.value)){
+        password_error.style.display = "block";
+    }else {
+        password_error.style.display = "none";
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function ValidateEmail(input) {
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    if (input.match(emailRegex)) {
+      return true;
+    } else {
+      return false;
+    }
+}
+function ValidatePassword(input) {
+    if(input.length>=4 && input.length<=60) {
+        return true;
+    }else{
+        return false;
+}
+}
